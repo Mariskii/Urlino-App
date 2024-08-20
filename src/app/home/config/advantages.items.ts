be4637bba8fabcfec8d4ae014acf12889bb1a246
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-20 23:25:18 +0200

    fix: responsive home

--- src/app/home/config/advantages.items.ts
+++ src/app/home/config/advantages.items.ts
@@ -10,7 +10,7 @@ export const advantageItems: Advantage[] = [
     description:'Shortened URLs are easier to remember and type, which is beneficial for marketing campaigns and sharing links in space-limited contexts. They simplify the inclusion of links in social media posts, text messages, and other constrained spaces, improving both usability and visual appeal.'
   },
   {
-    title:'Facilitate Sharing:',
+    title:'Facilitate Sharing',
     description:'Long and complex URLs can be cumbersome to share, especially on platforms with character limits like Twitter. Shortened URLs offer a more streamlined alternative, making it easier to include them in messages and posts. Their brevity not only helps in adhering to character restrictions but also improves readability and reduces the likelihood of errors when typing or copying links.'
   }
 ]
