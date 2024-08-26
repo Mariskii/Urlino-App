c7c1669ae78bab7b85ed1e7bdc4d9eff0e6d6654
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-26 18:13:50 +0200

    tests: url-card

--- src/app/user/components/url-card/url-card.component.spec.ts
+++ src/app/user/components/url-card/url-card.component.spec.ts
@@ -11,13 +11,37 @@ describe('UrlCardComponent', () => {
       imports: [UrlCardComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(UrlCardComponent);
     component = fixture.componentInstance;
+
+    component.urlUser = {
+      id: '1',
+      longUrl: 'string',
+      customUrl: 'string',
+      customBody: 'string'
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('setDeleteConfirmation works correctly', () => {
+    const spyDelete = spyOn(component.deleteUrl, 'emit');
+
+    component.setDeleteConfirmation();
+
+    expect(spyDelete).toHaveBeenCalledWith('1');
+  });
+
+  it('setEditDialog works correctly', () => {
+    const spyDelete = spyOn(component.editUrl, 'emit');
+
+    component.setEditDialog();
+
+    expect(spyDelete).toHaveBeenCalledWith('1');
   });
 });
