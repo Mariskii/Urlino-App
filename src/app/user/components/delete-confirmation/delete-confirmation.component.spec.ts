77cfe6f686d88a416e83c7e68420a190ad9a2929
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-26 17:48:22 +0200

    tests: delete-confirmation component

--- src/app/user/components/delete-confirmation/delete-confirmation.component.spec.ts
+++ src/app/user/components/delete-confirmation/delete-confirmation.component.spec.ts
@@ -1,17 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteConfirmationComponent } from './delete-confirmation.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('DeleteConfirmationComponent', () => {
+
+  const MockDialogRef = {
+    close: (confirm: boolean) => {}
+  }
+
   let component: DeleteConfirmationComponent;
   let fixture: ComponentFixture<DeleteConfirmationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeleteConfirmationComponent]
+      imports: [DeleteConfirmationComponent],
+      providers: [
+        {provide: MatDialogRef, useValue: MockDialogRef},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(DeleteConfirmationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +29,21 @@ describe('DeleteConfirmationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('close dialog works correctly', () => {
+    const spyClose = spyOn(MockDialogRef, 'close');
+
+    component.closeDialog();
+
+    expect(spyClose).toHaveBeenCalledWith(false);
+  });
+
+  it('confirmation works correctly', () => {
+    const spyClose = spyOn(MockDialogRef, 'close');
+
+    component.confirmation();
+
+    expect(spyClose).toHaveBeenCalledWith(true);
   });
 });
