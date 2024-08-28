939e98d66c3e5fa8dd467090899a4c47b4096a48
Vicent Paya <vicentpayaginer@gmail.com>
2024-08-28 18:59:49 +0200

    tests: user-page component

--- src/app/user/pages/user-page/user-page.component.spec.ts
+++ src/app/user/pages/user-page/user-page.component.spec.ts
@@ -1,14 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserPageComponent } from './user-page.component';
-import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../../core/services/AuthService/auth.service';
 import { UrlService } from '../../../core/services/url.service';
-import { of } from 'rxjs';
-import { DeleteConfirmationComponent } from '../../components/delete-confirmation/delete-confirmation.component';
+import { of, throwError } from 'rxjs';
 import { PageEvent } from '@angular/material/paginator';
 
-fdescribe('UserPageComponent', () => {
+describe('UserPageComponent', () => {
 
   const MockUrlService = {
     getUrlsByUserId: () => of(
@@ -89,6 +88,27 @@ fdescribe('UserPageComponent', () => {
     expect(component.shortUrl).toEqual('http://localhost:8080/api/string');
   });
 
+  it('shortCustomizedUrl works correctly when customBody includes /', () => {
+    component.longUrl = 'long';
+    component.customBody = '/custom'
+
+    component.shortCustomizedUrl();
+
+    expect(component.errorMessage).toEqual('The character "/" is not allowed');
+  });
+
+  it('shortCustomizedUrl works correctly when throws error', () => {
+    const errorResponse = { error: { message: 'Error' } };
+
+    const spyShortenCustomizedUrl = spyOn(MockUrlService, 'shortenCustomizedUrl').and.returnValue(throwError(errorResponse));
+
+    component.longUrl = 'long';
+
+    component.shortCustomizedUrl();
+
+    expect(spyShortenCustomizedUrl).toHaveBeenCalled();
+  });
+
   it('changePage works correctly', () => {
 
     const getUrlsByUserIdSpy = spyOn(component, 'getUrlsByUserId');
@@ -106,13 +126,50 @@ fdescribe('UserPageComponent', () => {
     expect(getUrlsByUserIdSpy).toHaveBeenCalledWith(2);
   });
 
+  it('getUrlsByUserId works correctly', () => {
+
+    const errorResponse = { error: { message: 'Error' } };
+
+    const spyGetUrlsByUserIdSpy = spyOn(MockUrlService, 'getUrlsByUserId').and.returnValue(throwError(errorResponse));
+
+
+    component.getUrlsByUserId(1);
+
+    expect(spyGetUrlsByUserIdSpy).toHaveBeenCalled();
+  });
+
   it('searchByShortUrl works correctly', () => {
-    const spyGetUrlsByUserIdAndShortUrl = spyOn(MockUrlService, 'getUrlsByUserIdAndShortUrl');
+    const spyGetUrlsByUserIdAndShortUrl = spyOn(MockUrlService, 'getUrlsByUserIdAndShortUrl').and.returnValue(of({
+      content: [
+          {
+              id: 'string',
+              longUrl: 'string',
+              customUrl: 'string',
+              customBody: 'string'
+          }
+      ],
+      totalElements: 2,
+      totalPages: 0
+  }));
 
     component.searchedUrl = 'short'
 
     component.searchByShortUrl();
 
     expect(spyGetUrlsByUserIdAndShortUrl).toHaveBeenCalled()
-  })
+  });
+
+  it('clearSearch works correctly', () => {
+    component.clearSearch();
+
+    expect(component.searchedUrl).toEqual('');
+  });
+
+  it('resetText works correctly', () => {
+    component.resetText();
+
+    expect(component.errorMessage).toEqual('');
+    expect(component.longUrl).toEqual('');
+    expect(component.customBody).toEqual('');
+  });
 });
